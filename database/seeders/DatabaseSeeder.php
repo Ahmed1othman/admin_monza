82ce6e4b9d21399c27f3a0b4fd3d3a3ddc28c857
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2024-11-29 21:12:13 +0200

    users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,21 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-             Settings::class,
-             SetupPermissions::class,
-             currencies::class,
-             ModelsContent::class,
-             Cities::class,
-             CompanyDescriptions::class,
-             CompaniesCities::class,
-        ]);
+//        $this->call([
+//             Settings::class,
+//             SetupPermissions::class,
+//             currencies::class,
+//             ModelsContent::class,
+//             Cities::class,
+//             CompanyDescriptions::class,
+//             CompaniesCities::class,
+//        ]);
         // \App\Models\User::factory(10)->create();
 
+        DB::table('users')->trancate();
          \App\Models\User::create([
-             'name' => 'ahmed',
-             'email' => 'ahmed@example.com',
-             'password' => '12345678',
+             'name' => 'admin',
+             'email' => 'admin@monzarrentalcar.com',
+             'password' => 'Monzer@2025',
              'type' => 'admin',
          ]);
     }
