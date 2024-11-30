6aaed724bbd896717b04bfdd6a45f6087f4813d1
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2024-12-01 00:20:02 +0200

    about us

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -21,10 +26,52 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
+
+    public function render($request, Throwable $exception)
+    {
+        return $this->handleApiException($request, $exception);
+    }
+
+    protected function handleApiException($request, $exception)
+    {
+        if ($exception instanceof AuthenticationException) {
+            return parent::render($request, $exception);
+        }
+
+        if ($exception instanceof ValidationException) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $exception->validator->errors(),
+            ], 422);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'message' => 'An error occurred',
+                'errors' => $exception->getMessage(),
+            ], 404);
+        }
+
+        if ($exception instanceof ModelNotFoundException ) {
+            return response()->json([
+                'message' => 'object not found',
+                'errors' => $exception->getMessage(),
+            ], 404);
+        }
+
+        if ($exception instanceof \Exception) {
+            return response()->json([
+                'message' => 'An error occurred',
+                'errors' => $exception->getMessage(),
+            ], 500);
+        }
+        return parent::render($request, $exception);
+    }
+
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            return $e->getMessage();
+            //
         });
     }
 }
